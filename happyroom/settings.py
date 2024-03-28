2c9d6a55d52bb431d7305a1875357a51541ed524
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-03-28 08:16:26 +0000

    initial commit

--- happyroom/settings.py
+++ happyroom/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lxnsf!2+=f=j6n#t)^!26o8js#vry%c&wl!ff*j^f_ka4hfm97'
+SECRET_KEY = 'django-insecure-a7i0y_d1)(p(pk2308_q3x39yt=b&nk5ezx*8z@fdxt&i+ly(#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-tildeholmqvis-happyroom-u5mmw5xs9i3.ws-eu110.gitpod.io']
 
 
 # Application definition
