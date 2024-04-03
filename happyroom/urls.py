d79b0b724835c7f1505c4eab94a9c1f0ae5037a7
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-04-03 08:43:09 +0000

    adding a faq template to root templates and an url to handle that request

--- happyroom/urls.py
+++ happyroom/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -23,5 +24,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('faq/', TemplateView.as_view(template_name='faq.html'), name='faq'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
