fe1af182a1626ba7ba3addded3bce5e7d1b6e524
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-04-03 10:10:48 +0000

    adding a profile app, same as in walkthrough

---
+++ profiles/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('profile/', views.profile_view, name='profile'),
+    path('order_history/<order_number>', views.order_history, name='order_history'),
+]
