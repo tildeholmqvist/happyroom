89bf9466d3171e1787972192795d5a057e286d33
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-04-04 11:28:18 +0000

    adding the checkout and bag functions with help from sourcecode from walkthrough project boutique ado

--- happyroom/urls.py
+++ happyroom/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('profile/', include('profiles.urls')),
+    path('checkout/', include('checkout.urls')),
     path('bag/', include('bag.urls')),
     path('faq/', TemplateView.as_view(template_name='faq.html'), name='faq'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
