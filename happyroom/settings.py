c6804578802ab475b090a845c161f48424386149
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-04-05 09:02:52 +0000

    adding a no image picture

--- happyroom/settings.py
+++ happyroom/settings.py
@@ -188,7 +188,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
-f 'DEVELOPMENT' in os.environ:
+if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'happyroom@contact.com'
 else:
