c981396533358dea8b49be27299387b0cc3d85b6
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-04-10 18:03:21 +0000

    adding env.py file

--- happyroom/settings.py
+++ happyroom/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
-CSRF_TRUSTED_ORIGINS = ['happy-room-e68715746875.herokuapp.com', 'localhost']
+CSRF_TRUSTED_ORIGINS = ['https://happy-room-e68715746875.herokuapp.com/', 'https://8000-tildeholmqv-pp5happyroo-f7tyuzz2jbt.ws-eu110.gitpod.io/']
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['happy-room-e68715746875.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['happy-room-e68715746875.herokuapp.com', '8000-tildeholmqv-pp5happyroo-f7tyuzz2jbt.ws-eu110.gitpod.io/']
 
 # Application definition
 
