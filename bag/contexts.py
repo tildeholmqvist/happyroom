782dd85b62a939642d152b24222f84763629b07d
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-04-11 11:45:17 +0000

    trying to go back to the things i changed, to make the site work again

--- bag/contexts.py
+++ bag/contexts.py
@@ -2,15 +2,16 @@ from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
+from services.models import Service
 
 def bag_contents(request):
-
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
+    booking_id = None  
 
-    for item_id, item_data in bag.items():
+    for item_id, item_data in bag.items(): 
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
@@ -21,7 +22,21 @@ def bag_contents(request):
                 'product': product,
             })
         else:
-            product = get_object_or_404(Product, pk=item_id)
+            service_id = item_data.get('service_id')
+            product = get_object_or_404(Service, pk=service_id)  
+            quantity = item_data['quantity']
+            total += quantity * product.price
+            product_count += quantity
+            bag_items.append({
+                'item_id': item_id,
+                'service_id': service_id,
+                'quantity': quantity,
+                'product': product,
+            })
+
+    for item_id, item_data in bag.items():
+        if not isinstance(item_data, int):
+            product = get_object_or_404(Service, pk=item_id)  
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
@@ -52,3 +67,4 @@ def bag_contents(request):
     }
 
     return context
+
--- services/models.py
+++ services/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-from products.models import Product
 
-class Service(Product):
+class Service(models.Model):
     type = models.CharField(max_length=250)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.type
@@ -18,7 +22,7 @@ class BookService(models.Model):
     message = models.TextField()
     email = models.EmailField()
     phone = models.CharField(max_length=20)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100) 
 
     def __str__(self):
         return f"{self.user.username} - {self.service.type} - {self.date_time} "
