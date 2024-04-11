7c1b63ce169be891585700d57845a563dd77b3cc
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-04-11 10:30:23 +0000

    changing the service model to inherit from product

--- services/models.py
+++ services/models.py
@@ -2,13 +2,10 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-class Service(models.Model):
+from products.models import Product
+
+class Service(Product):
     type = models.CharField(max_length=250)
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.type
@@ -24,7 +21,7 @@ class BookService(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.user.username} - {self.service.type} - {self.date_time}"
+        return f"{self.user.username} - {self.service.type} - {self.date_time} "
 
     class Meta:
         ordering = ['date_time']
--- services/urls.py
+++ services/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from bag import views as bag_views 
 
 urlpatterns = [
     path('', views.services, name='services'),
-    path('add/', views.add_service, name='add_service'),
+    path('add/<item_id>/', bag_views.add_to_bag, name='add_to_bag'),
     path('services/book_service/<int:service_id>/', views.book_service, name='book_service'),
     path('inspration/', TemplateView.as_view(template_name='inspiration.html'), name='inspiration'),
     path('service_confirmation/<int:booking_id>/', views.service_confirmation, name='service_confirmation'),
