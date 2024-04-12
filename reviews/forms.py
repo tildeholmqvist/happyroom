431f891addc29400e51fab36caf3b1d74d6a056a
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-04-12 11:25:15 +0000

    adding more layout and fixing the review form

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Review
 
 class ReviewForm(forms.ModelForm):
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Your name'}))
+    comment = forms.CharField(max_length=250, widget=forms.Textarea(attrs={'placeholder': 'Leave your feedback', 'cols': 50, 'rows': 5}))
     class Meta:
         model = Review
         fields = ['name', 'comment']
