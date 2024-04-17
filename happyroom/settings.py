061e3289be93fae4f60727c73ef4d3af8c9ae1c4
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-04-17 13:29:35 +0000

    deployment

--- happyroom/settings.py
+++ happyroom/settings.py
@@ -192,6 +192,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 if 'USE_AWS' in os.environ:
     #Cache control
     AWS_S3_OBJECT_PARAMETERS = {
@@ -215,8 +216,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
-
 
 
 # Default primary key field type
