e9f6bed906c44297c9d903b1a08153e5d1619e58
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-04-23 10:04:42 +0000

    writing the marketing and seo part of the readme

--- products/models.py
+++ products/models.py
@@ -43,7 +43,7 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     is_bestseller = models.BooleanField(default=False)
-    is_new_items = models.BooleanField(default=False)
+    is_new_items = models.BooleanField(default=False) 
     is_last_chance= models.BooleanField(default=False)
 
     def __str__(self):
