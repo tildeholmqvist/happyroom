5156d65e8cff6a52adfba464af24798ede1f1122
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-04-25 07:47:02 +0000

    adding more lyaout to product and cateogy pages

--- happyroom/settings.py
+++ happyroom/settings.py
@@ -217,7 +217,6 @@ if 'USE_AWS' in os.environ:
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
