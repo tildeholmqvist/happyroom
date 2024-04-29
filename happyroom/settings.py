e0307677903e38c12611d888da66d05409b902a0
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-04-29 10:13:06 +0000

    trying another approach to allow am i repsonsive to test the site

--- happyroom/settings.py
+++ happyroom/settings.py
@@ -63,13 +63,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
 ]
 
